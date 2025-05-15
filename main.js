3e8b6864d2398b7c4de79276992b72fee31ae89b
picspiii1234 <ibrihim539@gmail.com>
2025-05-15 22:15:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+alert ('boshlanishta arrining uzunligi 4')
+
+let data = ['olma', 'Banan', 'Gilos', 'Shoftoli', ];
+
+console.log(data);
+
+
+let a1 = confirm('sizning arrayingizdan malumotni olib tashlamoqchimiz');
+
+console.log(a1);
+
+if(a1){
+    console.log(data.pop());
+    console.log(data);
+    alert ('Arreya uzunligi 3')
+}else{
+    alert ('Arreaning uzunligi 4')
+    console.log(data);
+        
+}   
+
+
